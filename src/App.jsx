9b08efec3070cabd17e798937159bea6ad755e3e
Chandu <dulamchandu13@gmail.com>
2025-04-16 11:12:16 +0530

    Add Header NavBar with links and cart item count

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Header from "./components/Header";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/restaurant/:id" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,19 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
-import { auth } from "../firebase/firebaseConfig";
-import { signOut } from "firebase/auth";
+import { useSelector } from "react-redux";
 
 function Header() {
-  const { user } = useContext(UserContext);
-
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
+  const cartItems = useSelector((state) => state.cart);
 
   return (
     <header style={styles.header}>
-      <h1 style={styles.logo}>🍔 Food Delivery</h1>
+      <h1 style={styles.logo}>🍔 Foodie</h1>
       <nav style={styles.nav}>
         <Link to="/" style={styles.link}>
           Home
         </Link>
-        {!user ? (
-          <>
-            <Link to="/login" style={styles.link}>
-              Login
-            </Link>
-            <Link to="/signup" style={styles.link}>
-              Signup
-            </Link>
-          </>
-        ) : (
-          <>
-            <span style={styles.link}>{user.email}</span>
-            <button onClick={handleLogout} style={styles.button}>
-              Logout
-            </button>
-          </>
-        )}
+        <Link to="/cart" style={styles.link}>
+          Cart ({cartItems.length})
+        </Link>
       </nav>
     </header>
   );
@@ -42,7 +21,7 @@ function Header() {
 
 const styles = {
   header: {
-    backgroundColor: "#333",
+    backgroundColor: "#000",
     color: "#fff",
     padding: "1rem 2rem",
     display: "flex",
@@ -51,23 +30,16 @@ const styles = {
   },
   logo: {
     margin: 0,
+    fontSize: "1.5rem",
   },
   nav: {
     display: "flex",
-    gap: "1rem",
-    alignItems: "center",
+    gap: "1.5rem",
   },
   link: {
     color: "#fff",
     textDecoration: "none",
-    fontWeight: "bold",
-  },
-  button: {
-    backgroundColor: "transparent",
-    border: "1px solid #fff",
-    color: "#fff",
-    padding: "5px 10px",
-    cursor: "pointer",
+    fontSize: "1rem",
   },
 };
 
